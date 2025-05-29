652260ab94462af883b513e0ebd5803cdc30ed4f
MujkicAmar <lateboomer666@gmail.com>
2025-05-29 17:16:21 +0200

    First iteration

--- RAG/RAG_QUIZBOWL.py
+++ RAG/RAG_QUIZBOWL.py
@@ -2,10 +2,18 @@ import os
 import glob
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import CharacterTextSplitter
-
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
+from openai import OpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain_community.chat_models import ChatOllama
 
 model = "tinyllama"
+llm = ChatOllama(model=model)
 
+
+db_name = "vector_db"
 files = glob.glob("DOCUMENTS/*.pdf")
 
 pdf_documents = []
@@ -18,6 +26,22 @@ for document in files:
 text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
 chunks = text_splitter.split_documents(pdf_documents)
 
-for chunk in chunks:
-    if "Norfolk Southern" in chunk.page_content:
-        print(chunk)
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+if os.path.exists(db_name):
+    Chroma(persist_directory=db_name, embedding_function=embeddings).delete_collection()
+
+
+vectorstore = Chroma.from_documents(documents=chunks, embedding=embeddings, persist_directory=db_name)
+print(f"Vectorstore created with {vectorstore._collection.count()} documents")
+
+memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+
+retriever = vectorstore.as_retriever()
+
+conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=retriever, memory=memory)
+
+query = "Write 5 short Quizbowl-style questions based on the documents provided"
+result = conversation_chain.invoke({"question":query})
+print(result["answer"])
+
